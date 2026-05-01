84df2b64b83ec2dd24dea2fd74d7de2d52c6a130
pshrote1-afk <pshrote1@gmail.com>
2026-05-01 19:18:11 +0530

    *imp* add NavBar on all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,19 @@ import './App.css'
 import Home from './pages/Home'
 import Favorites from './pages/Favorites'
 import {Routes, Route} from "react-router-dom"
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/fav" element={<Favorites />}></Route>
-      </Routes>
-    </main>
+    <div>
+      <NavBar /> 
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/fav" element={<Favorites />}></Route>
+        </Routes>
+      </main>
+    </div>
   )
 }
 
